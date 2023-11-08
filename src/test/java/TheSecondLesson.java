d0d929f148dc93ff38e1683360c7a11ed85a15e3
viktorvojnov <gans13_07@mail.ru>
2023-11-08 23:13:18 +0500

    The ex.6

--- src/test/java/TheSecondLesson.java
+++ src/test/java/TheSecondLesson.java
@@ -1,5 +1,6 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
 
 public class TheSecondLesson {
@@ -10,5 +11,17 @@ public class TheSecondLesson {
                 .andReturn().jsonPath();
         String answer = response.getJsonObject("messages[1]").toString();
         System.out.println(answer);
+    }
+
+    @Test
+    public void getRedirectAddress() {
+        Response response = RestAssured
+                .given()
+                .redirects()
+                .follow(false)
+                .get("https://playground.learnqa.ru/api/long_redirect")
+                .andReturn();
+        String redirect = response.header("location").toString();
+        System.out.println(redirect);
     }
 }
