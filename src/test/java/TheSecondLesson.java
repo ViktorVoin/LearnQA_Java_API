ac9945694ef305f7aecd1a4856ea4cfe874e10cd
viktorvojnov <gans13_07@mail.ru>
2023-11-13 22:46:47 +0500

    The ex.8 brutForce pass

--- src/test/java/TheSecondLesson.java
+++ src/test/java/TheSecondLesson.java
@@ -1,8 +1,15 @@
-import groovyjarjarantlr4.v4.runtime.misc.NotNull;
+import groovy.json.JsonToken;
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+import java.util.Scanner;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -65,6 +72,26 @@ public class TheSecondLesson {
                 .get("https://playground.learnqa.ru/ajax/api/longtime_job")
                 .then()
                 .assertThat()
-                .body("status", equalTo("Job is ready"),"result",notNullValue());
+                .body("status", equalTo("Job is ready"), "result", notNullValue());
+    }
+
+    @Test
+    public void brutTokenData() throws IOException {
+        Scanner sc = new Scanner(new File("pass.txt"));
+        String brut = null;
+        String result = null;
+        while (!Objects.equals(result, "You are authorized")) {
+            brut = sc.nextLine();
+            String getCookie = given()
+                    .params("login", "super_admin")
+                    .params("password", brut)
+                    .post("https://playground.learnqa.ru/ajax/api/get_secret_password_homework")
+                    .getCookie("auth_cookie");
+            Response authData = given()
+                    .cookie("auth_cookie", getCookie)
+                    .post("https://playground.learnqa.ru/ajax/api/check_auth_cookie");
+            result = authData.body().asString();
+        }
+        System.out.println("Pass: " + brut + "\n" + "Server response body: " + result);
     }
 }
