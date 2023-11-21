07c81af41554da6a524388fd4c43265008c3a6f5
viktorvojnov <gans13_07@mail.ru>
2023-11-21 08:40:19 +0500

    The ex.11 and ex.12.

--- src/test/java/TheThirdLesson.java
+++ src/test/java/TheThirdLesson.java
@@ -1,5 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.apache.http.cookie.Cookie;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -13,5 +15,21 @@ public class TheThirdLesson {
         String answer = response.getJsonObject("messages[1]").toString();
         assertTrue(answer.length() > 15,"expected length of String 15 or more");
         System.out.println(answer);
+    }
+    @Test
+    public void assertCookie() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_cookie")
+                .andReturn();
+        String cookie = response.getCookie("HomeWork");
+        assertTrue(cookie.equals("hw_value"), "cookie is not valid");
+    }
+    @Test
+    public void assertHeader() {
+        Response response = RestAssured
+                .get("https://playground.learnqa.ru/api/homework_header")
+                .andReturn();
+        String header = response.getHeader("x-secret-homework-header");
+        assertTrue(header.equals("Some secret value"), "header is not valid");
     }
 }
